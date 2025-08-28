62395c412a495dc158a2ab4699e7eaaf7df1eeec
LaaAdam <adam.laaouane.pro@gmail.com>
2025-08-28 19:42:19 +0200

    ez

--- assets/js/app.js
+++ assets/js/app.js
@@ -104,3 +104,16 @@ function myFunction(screenSize) {
 AOS.init({
     duration: 800,
 });
+  // Désactive clic droit
+  document.addEventListener("contextmenu", e => e.preventDefault());
+
+  // Désactive certaines touches (F12, Ctrl+Shift+I, Ctrl+U)
+  document.onkeydown = function(e) {
+    if (
+      e.keyCode == 123 || // F12
+      (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) ||
+      (e.ctrlKey && e.keyCode == "U".charCodeAt(0))
+    ) {
+      return false;
+    }
+  }
